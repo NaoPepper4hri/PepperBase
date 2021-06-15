b7d020d86c3f19dde79e51678c6b2b2e2b75dbb3
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-06-15 16:02:41 +1000

    Added example animation.

--- app/src/main/java/com/example/pepperbase/MainActivity.kt
+++ app/src/main/java/com/example/pepperbase/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.pepperbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.`object`.actuation.Animate
+import com.aldebaran.qi.sdk.`object`.actuation.Animation
+import com.aldebaran.qi.sdk.builder.AnimateBuilder
+import com.aldebaran.qi.sdk.builder.AnimationBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
+private const val ROBOT_ANIMATION_TAG = "RobotAnimation"
 
 class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
+
+    private var animate: Animate? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +32,26 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     override fun onRobotFocusGained(qiContext: QiContext) {
         // The robot focus is gained.
+        val animation: Animation =
+            AnimationBuilder.with(qiContext).withResources(R.raw.hello_a003).build()
+
+        animate = AnimateBuilder.with(qiContext).withAnimation(animation).build()
+
+        animate?.addOnStartedListener { Log.i(ROBOT_ANIMATION_TAG, "Animation started.") }
+        val animateFut = animate?.async()?.run()
+        animateFut?.thenConsume { future ->
+            if (future.isSuccess) {
+                Log.i(ROBOT_ANIMATION_TAG, "Animation finished with success.")
+            } else if (future.hasError()) {
+                Log.e(ROBOT_ANIMATION_TAG, "Animation finished with error.", future.error)
+            }
+        }
     }
 
     override fun onRobotFocusLost() {
         // The robot focus is lost.
+        animate?.removeAllOnStartedListeners()
+
     }
 
     override fun onRobotFocusRefused(reason: String) {
