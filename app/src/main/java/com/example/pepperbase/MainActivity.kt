a816414bc625c242d039de610f0499cfbbb72de6
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-06-15 15:20:34 +1000

    Basic implementation for Pepper application.

--- app/src/main/java/com/example/pepperbase/MainActivity.kt
+++ app/src/main/java/com/example/pepperbase/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.pepperbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Register the RobotLifecycleCallbacks to this Activity.
+        QiSDK.register(this, this)
+    }
+
+    override fun onDestroy() {
+        // Unregister the RobotLifecycleCallbacks for this Activity.
+        QiSDK.unregister(this, this)
+        super.onDestroy()
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext) {
+        // The robot focus is gained.
+    }
+
+    override fun onRobotFocusLost() {
+        // The robot focus is lost.
+    }
+
+    override fun onRobotFocusRefused(reason: String) {
+        // The robot focus is refused.
     }
 }
