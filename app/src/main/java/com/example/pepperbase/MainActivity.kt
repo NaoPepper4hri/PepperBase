d63c3b6fccd1c8bd16aad044eba60199b339654a
Mr Aitor Miguel Blanco <aitormibl@gmail.com>
2021-09-29 14:36:40 +1000

    Added buttons to run animation and play speech

--- app/src/main/java/com/example/pepperbase/MainActivity.kt
+++ app/src/main/java/com/example/pepperbase/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.pepperbase
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.aldebaran.qi.sdk.QiContext
 import com.aldebaran.qi.sdk.QiSDK
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
 import com.aldebaran.qi.sdk.`object`.actuation.Animate
 import com.aldebaran.qi.sdk.`object`.actuation.Animation
+import com.aldebaran.qi.sdk.`object`.conversation.Phrase
+import com.aldebaran.qi.sdk.`object`.conversation.Say
 import com.aldebaran.qi.sdk.builder.AnimateBuilder
 import com.aldebaran.qi.sdk.builder.AnimationBuilder
+import com.aldebaran.qi.sdk.builder.SayBuilder
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
 private const val ROBOT_ANIMATION_TAG = "RobotAnimation"
@@ -19,6 +23,7 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         // Register the RobotLifecycleCallbacks to this Activity.
         QiSDK.register(this, this)
     }
@@ -31,20 +36,15 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     override fun onRobotFocusGained(qiContext: QiContext) {
         Thread.sleep(5000)
-        // The robot focus is gained.
-        val animation: Animation =
-            AnimationBuilder.with(qiContext).withResources(R.raw.bow_a001).build()
-
-        animate = AnimateBuilder.with(qiContext).withAnimation(animation).build()
-
-        animate?.addOnStartedListener { Log.i(ROBOT_ANIMATION_TAG, "Animation started.") }
-        val animateFut = animate?.async()?.run()
-        animateFut?.thenConsume { future ->
-            if (future.isSuccess) {
-                Log.i(ROBOT_ANIMATION_TAG, "Animation finished with success.")
-            } else if (future.hasError()) {
-                Log.e(ROBOT_ANIMATION_TAG, "Animation finished with error.", future.error)
-            }
+        val startAnimationButton: Button = findViewById(R.id.startAnimation)
+        val animation = buildAnimation(qiContext, R.raw.bow_a001)
+        startAnimationButton.setOnClickListener {
+            animation?.run()
+        }
+        val startSpeechButton: Button = findViewById(R.id.startSpeech)
+        val speech = buildSpeech(qiContext, "Hello world!")
+        startSpeechButton.setOnClickListener {
+            speech.run()
         }
     }
 
@@ -56,5 +56,24 @@ class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
 
     override fun onRobotFocusRefused(reason: String) {
         // The robot focus is refused.
+    }
+
+    private fun buildAnimation(qiContext: QiContext, ResourceId: Int): Animate.Async? {
+        // The robot focus is gained.
+        val animation: Animation =
+            AnimationBuilder.with(qiContext).withResources(ResourceId).build()
+
+        animate = AnimateBuilder.with(qiContext).withAnimation(animation).build()
+
+        animate?.addOnStartedListener { Log.i(ROBOT_ANIMATION_TAG, "Animation started.") }
+        return animate?.async()
+    }
+
+    private fun buildSpeech(qiContext: QiContext, text: String): Say.Async {
+        val phrase = Phrase(text)
+        val say: Say = SayBuilder.with(qiContext)
+            .withPhrase(phrase)
+            .build()
+        return say.async()
     }
 }
